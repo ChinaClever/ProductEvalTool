df37198db372a030069e8c13583ea08cbb98b917
luozhiyong <luozhiyong131@qq.com>
2026-06-26 14:49:10 +0800

    勾选bug

--- ProductEvalTool/qc-busbar/safety/test_thread.cpp
+++ ProductEvalTool/qc-busbar/safety/test_thread.cpp
@@ -34,7 +34,8 @@ void Test_Thread::timeoutDone()
 {
     if(mPro->oning)
     {
-        bool ret = mRead->readDevBus();
+//        bool ret = mRead->readDevBus();
+        bool ret = true;
         QString sendStr = "";
 
         if(!ret) {
